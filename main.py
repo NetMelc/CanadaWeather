14e4bd08d0edfc9821a8c7ca77bfa5fb0c4f128d
NetMelc <149643013+NetMelc@users.noreply.github.com>
2024-08-22 00:54:59 -0400

    update for hourly data - to fix
    Need to fix line 34 for range(1,13) for month variable

--- main.py
+++ main.py
@@ -6,32 +6,32 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def get_url(station_id, year):
-    return f'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={station_id}&Year={year}&Month=8&Day=14&timeframe=2&submit=Download+Data'
+def get_url(station_id, year,month):
+    return f'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={station_id}&Year={year}&Month={month}&Day=14&timeframe=1&submit=Download+Data'
 
-def get_filename(station_id, year):
-    return f'StationData/{station_id}/{station_id}_{year}.csv'
+def get_filename(station_id, year,month):
+    return f'StationData_hourly/{station_id}/{station_id}_{year}_{month}.csv'
 
-def is_data_fetched(station_id, year):
-    return os.path.isfile(get_filename(station_id, year))
+def is_data_fetched(station_id, year,month):
+    return os.path.isfile(get_filename(station_id, year,month))
 
 def get_weather_data_per_year(args):
-    station_id, year = args
+    station_id, year,month = args
     # print('Fetching', station_id, year)
-    url = get_url(station_id, year)
-    out_path = get_filename(station_id, year)
+    url = get_url(station_id, year,month)
+    out_path = get_filename(station_id, year,month)
 
-    os.makedirs(f'StationData/{station_id}', exist_ok=True)
+    os.makedirs(f'StationData_hourly/{station_id}', exist_ok=True)
 
     file = requests.get(url)
     with open(out_path, 'wb') as f:
         f.write(file.content)
     
-    # print('Done', station_id, year)
+    print('Done', station_id, year,month)
 
 
 df = pd.read_csv('stations.csv')
-args = [(row['Station ID'], year) for _, row in df.iterrows() for year in range(int(row['First Year']), int(row["Last Year"]) + 1) if not is_data_fetched(str(row['Station ID']), year) ]
+args = [(row['Station ID'], year,month) for _, row in df.iterrows() for year in range(int(row['HLY First Year']), int(row["HLY Last Year"]) + 1) if not is_data_fetched(str(row['Station ID']), year,month) ]
 
-with ThreadPoolExecutor(max_workers=100) as executor:
+with ThreadPoolExecutor(max_workers=1) as executor:
     results = list(tqdm(executor.map(get_weather_data_per_year, args), total=len(args)))
